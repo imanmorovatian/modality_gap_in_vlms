e95fd3a413364c82a891d050b3cd715d39536a0d
Iman Morovatian <mo.iman.1377@gmail.com>
2024-04-29 10:08:43 +0200

    complete read_dataset function for all datasets

--- utils/read_dataset.py
+++ utils/read_dataset.py
@@ -5,13 +5,29 @@ def read_dataset(dataset_name):
     img_data = {}
     txt_data = {}
 
-    if dataset_name == 'amazon_products':
+    if 'flickr30k' in dataset_name:
+        df = pd.read_csv(
+            'data/'+dataset_name+'.csv',
+            names=['img_id', 'caption', 'class_id', 'class'],
+            header=None)
+        print(df.head())
+        for key, table in df.groupby('class'):
+            img_data[key] = table['img_id'].to_list()
+            txt_data[key] = table['caption'].to_list()
+
+    elif 'mscoco' in dataset_name:
+        df = pd.read_csv('data/'+dataset_name+'.csv')
+        for key, table in df.groupby('class'):
+            img_data[key] = table['coco_url'].to_list()
+            txt_data[key] = table['caption'].to_list()
+    
+    elif 'amazon' in dataset_name:
         df = pd.read_csv('data/'+dataset_name+'.csv')
         for key, table in df.groupby('class'):
             img_data[key] = table['imageURLHighRes'].to_list()
             txt_data[key] = table['title'].to_list()
-
+            
     else:
-        raise Exception('Not implemented yet')
+        raise ValueError('Invalid dataset name!')
     
     return img_data, txt_data

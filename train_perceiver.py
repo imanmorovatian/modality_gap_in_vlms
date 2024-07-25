8e64e53de01052c7861d9057dc9dad4c41b895a1
Iman Morovatian <mo.iman.1377@gmail.com>
2024-07-25 16:22:28 +0200

    update the path in which perceiver is saved. Now, it saved in pkgs/Perceiver

--- train_perceiver.py
+++ train_perceiver.py
@@ -44,7 +44,7 @@ else:
     raise ValueError('The selected dataset is not supported')
 
 
-result_dir = f'results/perceiver_checkpoint/{dataset}'
+result_dir = f'pkgs/Perceiver'
 if not os.path.exists(result_dir):
     os.makedirs(result_dir)
 

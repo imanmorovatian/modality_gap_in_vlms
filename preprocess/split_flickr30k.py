88dbe61574d532ce012be9e62931fd43200cef53
Iman Morovatian <mo.iman.1377@gmail.com>
2024-08-16 00:39:33 +0200

    Add all captions for each image

--- preprocess/split_flickr30k.py
+++ preprocess/split_flickr30k.py
@@ -2,21 +2,20 @@ import numpy as np
 
 
 with open('data/annotations/flickr30k/results_20130124.token', 'r') as f:
-    temp = {}
+    image_captions = {}
+
     for line in f:
         img_id, cap = line.split('\t')
         img_id = img_id[:-2]
-        temp.setdefault(img_id, [])
-        temp[img_id].append(cap)
+        image_captions.setdefault(img_id, [])
+        image_captions[img_id].append(cap)
 
-    data = []
-    for img_id, captions in temp.items():
-        data.append( f'{img_id}#1\t{np.random.choice(captions, size=1)[0]}' )
+    images = list(image_captions.keys())
 
-    total = len(data)
+    total = len(images)
     train = (total // 5) * 3
     val = total // 5
-    test = total - train -val
+    test = total - train - val
 
     all_indices = list(range(total))
     train_indices= np.random.choice(all_indices, size=train, replace=False)
@@ -29,13 +28,19 @@ with open('data/annotations/flickr30k/results_20130124.token', 'r') as f:
 
     with open('data/annotations/flickr30k/train.token', 'w') as train_file:
         for idx in train_indices:
-            train_file.write(data[idx])
+            image_id = images[idx]
+            for cap_num, cap in enumerate(image_captions[image_id], start=1):
+                train_file.write( f'{image_id}#{cap_num}\t{cap}' )
 
     with open('data/annotations/flickr30k/val.token', 'w') as val_file:
         for idx in val_indices:
-            val_file.write(data[idx])
+            image_id = images[idx]
+            for cap_num, cap in enumerate(image_captions[image_id], start=1):
+                val_file.write( f'{image_id}#{cap_num}\t{cap}' )
 
     with open('data/annotations/flickr30k/test.token', 'w') as test_file:
         for idx in test_indices:
-            test_file.write(data[idx])
+            image_id = images[idx]
+            for cap_num, cap in enumerate(image_captions[image_id], start=1):
+                test_file.write( f'{image_id}#{cap_num}\t{cap}' )
 

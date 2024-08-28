611a5822bd5deb02649d24b94289967ffe3d81a9
Iman Morovatian <mo.iman.1377@gmail.com>
2024-08-28 15:57:53 +0200

    change name of train_perceiver.sh to train.sh

--- experiment_scripts/train.sh
+++ experiment_scripts/train.sh
@@ -3,11 +3,11 @@
 #SBATCH --mail-user=telegram:264754907
 #SBATCH --gpus=1
 #SBATCH --ntasks=1
-#SBATCH --time=26:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=4096
-#SBATCH --job-name=train_perceiver_conceptualCaptions
+#SBATCH --job-name=train_perceiver_flickr30k
 #SBATCH --output=terminal.txt
 
 source VLPs/bin/activate
 export WANDB_API_KEY=4af4c9a807cc6c8b208f4388909fc7f9ead825f0
-python3 train_perceiver.py --dataset ConceptualCaptions --batch_size 128 --no_epochs 50
+python3 train_perceiver.py --dataset flickr30k --batch_size 128 --no_epochs 50

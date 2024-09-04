e807af897a8d295d8a9d0e02ce913cd1e14c5bc7
Iman Morovatian <mo.iman.1377@gmail.com>
2024-09-04 21:53:31 +0200

    Edit BridgeTower to be compatible

--- models/custom_bridgetower.py
+++ models/custom_bridgetower.py
@@ -1,10 +1,7 @@
-from PIL import Image
 import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from transformers import RobertaTokenizerFast, BridgeTowerImageProcessor, BridgeTowerModel
-
-from tqdm import tqdm
 
 
 class CustomBridgeTower():
@@ -14,35 +11,45 @@ class CustomBridgeTower():
         self.image_preprocessor = BridgeTowerImageProcessor.from_pretrained("BridgeTower/bridgetower-base")
         self.model = BridgeTowerModel.from_pretrained('BridgeTower/bridgetower-base')
         self.model = self.model.to(self.device)
-        
-        self.name = 'BridgeTower'
-        
         self.transform = transforms.Compose([
                 transforms.Lambda(lambda img: torch.tensor(self.image_preprocessor.preprocess(img)['pixel_values']) ),
                 transforms.Resize((288, 288))
             ])
+        
+        self.name = 'BridgeTower'
     
-    def encode(self, dataset, batch_size):
-        dataloader = DataLoader(dataset, batch_size=batch_size)
-
+    def encode(self, dataloader):
+        self.model.eval()
         image_features = []
         text_features = []
 
         with torch.no_grad():
-            for batch in tqdm(dataloader):
+            for batch in dataloader:
                 images, text = batch
 
-                text = text[0]
-                text = self.text_tokenizer(text, padding=True, truncation=True, return_tensors='pt')
+                no_captions = text['input_ids'].size()[1]
                 text = text.to(self.device)
+                text_embeds = []
 
                 images = torch.squeeze(images)
+                if len(images.size()) == 3:
+                    images = images.unsqueeze(0)
                 images = images.to(self.device)
 
-                features = self.model(input_ids=text['input_ids'], attention_mask=text['attention_mask'], pixel_values=images)
+                for i in range(no_captions):
+                    input_temp = {key: value[:,i,:] for key, value in text.items()}
+                    temp = self.model(input_ids=input_temp['input_ids'],
+                                      attention_mask=input_temp['attention_mask'],
+                                      pixel_values=images)
+                    text_embeds.append(temp['text_features'][:,0,:])
+                
+                text_embeds = torch.vstack(text_embeds)
+                text_features.append(text_embeds)
 
-                text_features.append(features['text_features'][:,0,:])
-                image_features.append(features['image_features'][:,0,:])
+                img_embeds = self.model(input_ids=text['input_ids'][:,0,:],
+                                      attention_mask=text['attention_mask'][:,0,:],
+                                      pixel_values=images)['image_features'][:,0,:]
+                image_features.append(img_embeds)
 
             text_features = torch.vstack(text_features)
             text_features = torch.nn.functional.normalize(text_features, p=2.0, dim=1)

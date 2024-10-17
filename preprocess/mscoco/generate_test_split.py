ae69471f3880909353bc9baeee0a45f06b25951d
Iman Morovatian <mo.iman.1377@gmail.com>
2024-10-17 12:34:28 +0200

    Generate annotation files for mscoco
    Previously, just one caption was sampled for each image, but now 5 ones are sampled

--- preprocess/mscoco/generate_test_split.py
+++ preprocess/mscoco/generate_test_split.py
@@ -1,71 +1,113 @@
 import json
-import pandas as pd
-import random
+from collections import defaultdict
+import numpy as np
 
 
-with open('data/annotations/mscoco_val2017/val2017_captions.json') as f:
+with open('data/annotations/mscoco/val2017_captions.json') as f:
     data = json.load(f)
     no_test_samples = len(data['images'])
 
 
-sampled_data = {}
-with open('data/annotations/mscoco_val2017/train2017_captions.json') as f:
+train_data = {}
+test_data = {}
+with open('data/annotations/mscoco/train2017full_captions.json') as f:
     data = json.load(f) # a dict with keys: info, licenses, images, annotations
-    sampled_data['info'] = data['info']
-    sampled_data['licenses'] = data['licenses']
+    
+    train_data['info'] = data['info']
+    train_data['licenses'] = data['licenses']
+    
+    test_data['info'] = data['info']
+    test_data['licenses'] = data['licenses']
 
-    captions = data['annotations'] # is a list in which each element (dict) has image_id --> common, (id: id of caption), caption
-    df_captions = pd.DataFrame(captions)
-
-    images = data['images'] # s a list in which each element (dict) has license, file_name, coco_url, height, width, date_captured,
-                                # flickr_url, (id: id of image) --> common
-    df_images = pd.DataFrame(images)
-
-    df_img_cap = pd.merge(df_images,
-                          df_captions.drop_duplicates(subset='image_id'),
-                          left_on='id', right_on='image_id', suffixes=('_image', '_caption'))
-
-    samples = df_img_cap.sample(n=no_test_samples, replace=False)
-    samples = samples.to_dict(orient='records')
-
-    list_images = []
-    list_annotations = []
-
-    for s in samples:
-        obj_image = {
-            'license': s['license'],
-            'file_name': s['file_name'],
-            'coco_url': s['coco_url'],
-            'height': s['height'],
-            'width': s['width'],
-            'date_captured': s['date_captured'],
-            'flickr_url': s['flickr_url'],
-            'id': s['id_image'],
+    
+    img_info = {}
+    # a list in which each element (dict) has license, file_name, coco_url, height, width, date_captured,
+    # flickr_url, (id: id of image) --> common
+    for info in data['images']:
+        img_info[ info['id'] ] = {
+            'license': info['license'],
+            'file_name': info['file_name'],
+            'coco_url': info['coco_url'],
+            'height': info['height'],
+            'width': info['width'],
+            'date_captured': info['date_captured'],
+            'flickr_url': info['flickr_url']
         }
 
-        list_images.append(obj_image)
-        images.remove(obj_image)
-        
-        obj_caption = {
-            'image_id': s['image_id'],
-            'id': s['id_caption'],
-            'caption': s['caption']
+    img_cap = defaultdict(lambda: {'caption_ids': [], 'captions': []})
+    # a list in which each element (dict) has image_id --> common, (id: id of caption), caption
+    for info in data['annotations']:
+        img_cap[ info['image_id'] ]['caption_ids'].append(info['id'])
+        img_cap[ info['image_id'] ]['captions'].append(info['caption'])
+
+    
+    test_img_ids = list(map(int, np.random.choice(list(img_info.keys()), size=no_test_samples, replace=False)))
+    train_img_ids = [img_id for img_id in list(img_info.keys()) if img_id not in test_img_ids]
+
+    
+    captions = []
+    imgs = []
+    for img_id in train_img_ids:
+        img_obj = {
+            'id': img_id,
+            'license': img_info[img_id]['license'],
+            'file_name': img_info[img_id]['file_name'],
+            'coco_url': img_info[img_id]['coco_url'],
+            'height': img_info[img_id]['height'],
+            'width': img_info[img_id]['width'],
+            'date_captured': img_info[img_id]['date_captured'],
+            'flickr_url': img_info[img_id]['flickr_url'],  
         }
 
-        list_annotations.append(obj_caption)
-        captions.remove(obj_caption)
+        imgs.append(img_obj)
 
-    sampled_data['images'] = list_images
-    sampled_data['annotations'] = list_annotations
+        for caption_id, caption in zip(img_cap[img_id]['caption_ids'], img_cap[img_id]['captions']):
+            cap_obj = {
+                'image_id': img_id,
+                'id': caption_id,
+                'caption': caption
+            }
 
-    data['images'] = images
-    data['annotations'] = captions
+            captions.append(cap_obj)
+
+    train_data['images'] = imgs
+    train_data['annotations'] = captions
 
 
-    with open('data/annotations/mscoco_val2017/test2017_captions.json', 'w') as fp:
-        json.dump(sampled_data, fp)
+    captions = []
+    imgs = []
+    for img_id in test_img_ids:
+        img_obj = {
+            'id': img_id,
+            'license': img_info[img_id]['license'],
+            'file_name': img_info[img_id]['file_name'],
+            'coco_url': img_info[img_id]['coco_url'],
+            'height': img_info[img_id]['height'],
+            'width': img_info[img_id]['width'],
+            'date_captured': img_info[img_id]['date_captured'],
+            'flickr_url': img_info[img_id]['flickr_url'],  
+        }
 
-    with open('data/annotations/mscoco_val2017/train2017edited_captions.json', 'w') as fp:
-        json.dump(data, fp)
+        imgs.append(img_obj)
+
+        for caption_id, caption in zip(img_cap[img_id]['caption_ids'], img_cap[img_id]['captions']):
+            cap_obj = {
+                'image_id': img_id,
+                'id': caption_id,
+                'caption': caption
+            }
+
+            captions.append(cap_obj)
+
+    test_data['images'] = imgs
+    test_data['annotations'] = captions
+
+
+
+with open('data/annotations/mscoco/test2017_captions.json', 'w') as fp:
+    json.dump(test_data, fp)
+
+with open('data/annotations/mscoco/train2017_captions.json', 'w') as fp:
+    json.dump(train_data, fp)
 
     

2ed278fb25306981ed2b1a8446c6f8beed1bd2a3
Iman Morovatian <mo.iman.1377@gmail.com>
2025-01-15 12:03:44 +0100

    Complete the scripts related to SIMAT

--- simat.py
+++ simat.py
@@ -1,5 +1,5 @@
 import os
-import pandas as pd
+import csv
 import argparse
 
 import torch
@@ -122,6 +122,9 @@ def parse_args():
     parser.add_argument("--local-weights", type=str, required=False,
                         help='the local path in which the pretrained weights are saved', dest='PATH')
     parser.add_argument('--batch_size', type=int, required=True, help='batch size', dest='BATCH_SIZE')
+    parser.add_argument('--n', nargs='+', default=[1], help='list of values for the number of neighbors in the retrieval', dest='N')
+    parser.add_argument('--lambda', nargs='+', default=[1], help='list of values for lambda', dest='LAMBDA')
+    parser.add_argument('--domain', type=str, default='test', help='domain, test or dev', dest='DOMAIN')
     parser.add_argument('--save-embds', action='store_true', help='whether to save the embeddings of images and text', dest='SAVE_EMBDS')
 
     args = parser.parse_args()
@@ -133,6 +136,9 @@ args = parse_args()
 MODEL = args.MODEL
 PATH = args.PATH
 BATCH_SIZE = args.BATCH_SIZE
+N = [int(n) for n in args.N]
+LAMBDA = [float(l) for l in args.LAMBDA]
+DOMAIN = args.DOMAIN
 SAVE_EMBDS = args.SAVE_EMBDS # whether to save the embeddings of images and text
 NUM_WORKERS = 2
 
@@ -171,27 +177,52 @@ if SAVE_EMBDS:
     torch.save(img_enc_mapping, os.path.join(result_dir, 'image.pt'))
     torch.save(w2we, os.path.join(result_dir, 'image.ptd'))
 
-heads = torch.load('weights/simat/CLIP/head_clip_t=0.1.pt')
-image_head = heads['img_head']
-text_head = heads['txt_head']
+# heads = torch.load('weights/simat/CLIP/head_clip_t=0.1.pt')
+# image_head = heads['img_head']
+# text_head = heads['txt_head']
 # image_head = nn.Linear(512, 512)
 # image_head.load_state_dict(torch.load('weights/simat/CLIP/ViT32_LlLl_clip_loss_mscoco_img_head.pth'))
 # text_head = nn.Linear(512, 512)
 # text_head.load_state_dict(torch.load('weights/simat/CLIP/ViT32_LlLl_clip_loss_mscoco_txt_head.pth'))
 
-scores = compute_simiat_scores(image_head, text_head, img_enc_mapping, w2we)
-
-result_dir = f'results/simat/{MODEL}/'
-if not os.path.exists(result_dir):
-    os.makedirs(result_dir)
+scores = compute_simiat_scores(
+    img_enc_mapping, w2we, image_head=None, text_head=None,
+    domain=DOMAIN, lbds=LAMBDA, top_k=N)
 
 output = {
     'lambda': [],
+    'n': [],
     'score': []
 }
 
-for lambda_, score in scores.items():
+for (lambda_, n), score in scores.items():
     output['lambda'].append(lambda_)
+    output['n'].append(n)
     output['score'].append(score)
 
-pd.DataFrame(output).to_csv(os.path.join(result_dir, 'simat_score.csv'), index=False)
+
+result_dir = f'results/simat'
+if not os.path.exists(result_dir):
+    os.makedirs(result_dir)
+
+file_name = os.path.join(result_dir, 'simat_scores.csv')
+# writing column names
+if not os.path.exists(file_name):
+    with open(file_name, 'w', encoding='UTF8') as f:
+        rows = ['model', 'path', 'n', 'lambda', 'score']        
+        writer = csv.writer(f)
+        writer.writerow(rows)
+
+# writing column values
+with open(file_name, 'a', encoding='UTF8') as f:
+    for i in range(len(output['score'])):
+        rows = [
+            MODEL,
+            PATH if PATH is not None else 'None',
+            output['n'][i],
+            output['lambda'][i],
+            output['score'][i]
+        ]
+
+        writer = csv.writer(f)
+        writer.writerow(rows)
--- utils/metrics/simat_score.py
+++ utils/metrics/simat_score.py
@@ -5,10 +5,18 @@ import pandas as pd
 import numpy as np
 
 
+def take_topk(candidates, target, top_k):
+    tops = [r for r in candidates if r.item() != target]
+
+    return tops[:top_k]
+
+
 def make_normalized(x):
     return x/x.norm(dim=-1, keepdim=True)
 
-def compute_simiat_scores(image_head, text_head, image_embeddings, words_embeddings, domain='test', lbds=[1,]):
+
+def compute_simiat_scores(image_embeddings, words_embeddings, image_head=None, text_head=None,
+                          domain='test', lbds=[1,], top_k=[1,]):
     # Adopted from https://github.com/facebookresearch/SIMAT/blob/main/eval.py
 
     output = {}
@@ -22,26 +30,45 @@ def compute_simiat_scores(image_head, text_head, image_embeddings, words_embeddi
     transfos_did = [rid2did[rid] for rid in transfos.region_id]
     
     img_embs_stacked = torch.stack([image_embeddings[did2rid[i]] for i in range(len(image_embeddings))]).float()
-    img_embs_stacked = image_head(img_embs_stacked)
+    if image_head:
+        img_embs_stacked = image_head(img_embs_stacked)
     img_embs_stacked = make_normalized(img_embs_stacked)
     value_embs = torch.stack([img_embs_stacked[did] for did in transfos_did])
     
-    w2v = {k:make_normalized(text_head(v.float())) for k, v in words_embeddings.items()}
+    if text_head:
+        w2v = {k:make_normalized(text_head(v.float())) for k, v in words_embeddings.items()}
+    else:
+        w2v = {k:make_normalized(v.float()) for k, v in words_embeddings.items()}
     delta_vectors = torch.stack([w2v[x.target] - w2v[x.value] for i, x in transfos.iterrows()])
     
     oscar_scores = torch.load('data/annotations/simat/oscar_similarity_matrix.pt')
     weights = 1/np.array(transfos.norm2)**.5
     weights = weights/sum(weights)
-    
+
     for lbd in lbds:
-        target_embs = value_embs + lbd*delta_vectors
+        for k in top_k:
+            target_embs = value_embs + lbd*delta_vectors
 
-        nnb = (target_embs @ img_embs_stacked.T).topk(5).indices
-        nnb_notself = [r[0] if r[0].item() != t else r[1] for r, t in zip(nnb, transfos_did)]
-        
-        scores = np.array([oscar_scores[ri, tc] for ri, tc in zip(nnb_notself, transfos.target_ids)]) > .5
+            nnb = (target_embs @ img_embs_stacked.T).topk(2*k).indices
+            nnb_notself = [take_topk(r, t, k) for r, t in zip(nnb, transfos_did)]
 
-        output[lbd] = 100*np.average(scores, weights=weights)
+            scores = []
+            for ri, tc in zip(nnb_notself, transfos.target_ids):
+                temp = []
+                for rii in ri:
+                    if oscar_scores[rii.item(), tc] > 0.5:
+                        temp.append(True)
+                    else:
+                        temp.append(False)
+
+                temp = any(temp)
+                if temp:
+                    scores.append(1)
+                else:
+                    scores.append(0)
+            
+            
+            output[(lbd, k)] = 100*np.average(scores, weights=weights)
 
         
     return output
